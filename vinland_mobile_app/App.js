54f08bcb7802844a0ad0a0ff69e8f2c1525617f7
trongpham99-cpu <trong.phamtranduc@gmail.com>
2023-04-12 22:25:49 +0700

    update setup

--- vinland_mobile_app/App.js
+++ vinland_mobile_app/App.js
@@ -1,18 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import styles from './src/pages/home/style';
-
+import { Button } from 'react-native-paper';
 //components
 import { Home } from './src/pages/home/index';
 export default function App() {
   const title = "WELCOME TO VINLAND APP";
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Home  
-        title={title}
-      />
+      <Home title={title} />
     </View>
   );
 }
--- vinland_mobile_app/src/pages/home/index.js
+++ vinland_mobile_app/src/pages/home/index.js
@@ -1,11 +1,7 @@
 import styles from "./style";
-
+import { Text, View } from 'react-native';
 export const Home = (props) => {
     const { title } = props;
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title} >{title}</Text>
-        </View>
-    );
+    return <Text style={styles.title} >{title}</Text>
 }
 
