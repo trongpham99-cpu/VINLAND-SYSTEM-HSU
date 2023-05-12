862f8197d275ba4eb6f1c87e41ea5d1968bc7fd3
Duc Trong Pham <trong.phamtranduc@gmail.com>
2023-05-12 10:28:25 +0700

    update model home

--- vinland_be/src/models/modelHome.js
+++ vinland_be/src/models/modelHome.js
@@ -1,37 +1,62 @@
 const mongoose = require("mongoose");
-const homeSchema = new mongoose.Schema({
-  Title: {
-    type: String,
-    required: true,
+const homeSchema = new mongoose.Schema(
+  {
+    Title: {
+      type: String,
+      required: true,
+    },
+    Description: {
+      type: String,
+      required: true,
+    },
+    Price: {
+      type: String,
+      required: true,
+    },
+    Location: {
+      type: Object,
+      required: true,
+    },
+    Thumbnail: {
+      type: Object,
+      required: true,
+    },
+    Slug: {
+      type: String,
+    },
+    Rating: {
+      type: Number,
+      default: 0,
+      min: [0, 'Rating must be at least 0'],
+      max: [5, 'Rating must be at most 5'],
+      set: v => Math.round(v * 10) / 10,
+    },
+    Attachments: {
+      type: Array,
+      default: [],
+    },
+    Note: {
+      type: String,
+    },
+    Status: {
+      type: String,
+      required: true,
+    },
+    Owner: { //đăng bởi ai
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    //for actions
+    Comments: {
+      type: Array,
+      default: [],
+    }
   },
-  Description: {
-    type: String,
-    required: true,
-  },
-  Price: {
-    type: String,
-    required: true,
-  },
-  Location: {
-    type: String,
-    required: true,
-  },
-  Attechments: {
-    type: Array,
-    default: [],
-  },
-  Note: {
-    type: String,
-  },
-  Status: {
-    type: String,
-    required: true,
-  },
-  // User: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: "User",
-  // },
-});
+  {
+    timestamps: true,
+  }
+);
 
 let Home = mongoose.model("Home", homeSchema);
 module.exports = { Home };
